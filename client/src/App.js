72e578afa5e1e8aed26126a3d5cb809673a324de
Mario Sánchez <mr.mario.sm@gmail.com>
2019-01-05 14:46:13 +0100

    Performa a simple connection to the GraphQL server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
-
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3500/graphql'
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getBeers {
+          id
+          name
+          reviewer {
+            name
+            id
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 class App extends Component {
   render() {
