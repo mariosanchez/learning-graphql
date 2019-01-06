eb2d5478a66c9fa07a043791ac61963e7e85fbbe
Mario Sánchez <mr.mario.sm@gmail.com>
2019-01-06 21:03:16 +0100

    Guard null cases in BeerCard

--- client/src/components/BeerCard/BeerCard.js
+++ client/src/components/BeerCard/BeerCard.js
@@ -3,12 +3,10 @@ import './BeerCard.css';
 
 const BeerCard = ({ name, picture, brewery, reviewer }) => (
   <article className="BeerCard">
-    <img src={picture} alt={name} />
+    {picture && <img src={picture} alt={name} />}
     <h2>{name}</h2>
-    <h3>{brewery.name}</h3>
-    <p>
-      <small>Reviewed by {reviewer.name}</small>
-    </p>
+    {brewery && <h3>{brewery.name}</h3>}
+    <p>{reviewer && <small>Reviewed by {reviewer.name}</small>}</p>
   </article>
 );
 
