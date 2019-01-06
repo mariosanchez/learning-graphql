a7867707883287973505c5a0776d00050af42983
Mario Sánchez <mr.mario.sm@gmail.com>
2019-01-06 22:19:23 +0100

    Implement Login route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import Home from './Home/Home';
+import Login from './Login/Login';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3500/graphql'
@@ -13,6 +14,7 @@ const App = () => (
     <Router>
       <div className="App">
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
       </div>
     </Router>
   </ApolloProvider>
